b3fc692db389b94d9e0229bce1990207fd7a7aea
Dcris <18218552814@163.com>
2024-03-24 00:04:44 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="HomePage">
+    <el-input v-model="inputVal" style="width: 240px" placeholder="Please input" />
+    {{ inputVal }}
+    <el-button @click="clickButton">点击</el-button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { ElInput, ElButton } from "element-plus";
+const inputVal = ref("")
+function clickButton() {
+  console.log(inputVal.value);
+}
+</script>
+
+<style lang="less" scoped>
+.HomePage {
+  width: 100%;
+}
+</style>
