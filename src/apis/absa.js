18d85b0204deb487f906a36d363a930ce1cb199e
hyyBubbly <1270780922@qq.com>
2024-03-28 21:32:24 +0800

    progress:加一个接口

--- src/apis/absa.js
+++ src/apis/absa.js
@@ -4,6 +4,10 @@ export function ATE(sentence) {
     return service.post(`/absa/ate`,{ raw_text: sentence })
 }
 
-export function returnATSC(sentence) {
-    return service.post(`/absa/atsc`,{ raw_text: sentence })
+export function ATSC(sentence,term) {
+    return service.post(`/absa/atsc`,{ raw_text: sentence,aspect_term: term })
+}
+
+export function ASPE(sentence) {
+    return service.post(`/absa/aspe`,{ raw_text: sentence })
 }
